cf4fc7696d969a09af8718cf67871a3e0db1c3d8
jiayue-li <jiayue.li@berkeley.edu>
2018-03-12 15:54:59 -0700

    fixed confusion mat

--- random_forest_classifier.py
+++ random_forest_classifier.py
@@ -17,15 +17,17 @@ import numpy as np
 train_ratio = 0.6
 
 #Loads in data, separates out species labels
-bee_data = pd.read_csv('sample-data.csv')
-labels = bee_data['Species']
+bee_data = pd.read_csv('bee_info.csv')
+labels = bee_data['species']
 
 # #Splits up the bee_data into training and test set, factorizes it into numbers, instead of species
 bee_data['is_train'] = np.random.uniform(0, 1, len(bee_data)) <= train_ratio
 train_data, test_data = bee_data[bee_data['is_train']==True], bee_data[bee_data['is_train']==False]
-features = bee_data.columns[:4]
-training_labels, testing_labels = pd.factorize(train_data['Species'])[0], pd.factorize(test_data['Species'])[0]
+features = bee_data.columns[:3]
+factorized = pd.factorize(train_data['species'])
+training_labels, testing_labels, classes = factorized[0], pd.factorize(test_data['species'])[0], factorized[1]
 train, test = train_data[features], test_data[features]
+
 
 # #Creates a RandomForestClassifier, fits it on training data, and predicts on test data
 rf = RandomForestClassifier(random_state=42)
@@ -36,9 +38,9 @@ predictions = rf.predict(test)
 accuracyRF = accuracy_score(testing_labels, predictions)
 print("Random Forest Classifier Accuracy: ", accuracyRF)
 
-# #Displays a confusion of classified species
-print("Confusion Matrix: ")
-preds = features[predictions]
-testing_labs = features[testing_labels]
+#Displays a confusion of classified species
+print("\nConfusion Matrix: ")
+preds = classes[predictions]
+testing_labs = classes[testing_labels]
 confusion_mat = pd.crosstab(testing_labs, preds, rownames = ['Actual Species'], colnames = ['Predicted Species'])
 print(confusion_mat)
