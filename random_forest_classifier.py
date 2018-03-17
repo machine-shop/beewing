c098711268a87d2154fb5ea32610c6652cea951b
jiayue-li <jiayue.li@berkeley.edu>
2018-03-17 14:28:26 -0700

    added cross validation to model

--- random_forest_classifier.py
+++ random_forest_classifier.py
@@ -10,11 +10,17 @@ import sklearn
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+# from sklearn import svm
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import ShuffleSplit
 import pandas as pd
 import numpy as np
 
 
-train_ratio = 0.6
+
+train_ratio = 0.8
 
 #Loads in data, separates out species labels
 bee_data = pd.read_csv('bee_info.csv')
@@ -43,4 +49,29 @@ print("\nConfusion Matrix: ")
 preds = classes[predictions]
 testing_labs = classes[testing_labels]
 confusion_mat = pd.crosstab(testing_labs, preds, rownames = ['Actual Species'], colnames = ['Predicted Species'])
-print(confusion_mat)
+print(confusion_mat, "\n\n")
+
+#Cross Validation
+print("Performing Cross Validation")
+target = bee_data['species']
+data = bee_data[bee_data.columns[:3]]
+
+# X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=0)
+#
+# rf = RandomForestClassifier(random_state=42)
+# rf.fit(X_train, y_train)
+# accuracy = rf.score(X_test, y_test)
+# print(accuracy)
+#
+# clf = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)
+# print(clf.score(X_test, y_test))
+
+rfc = RandomForestClassifier(random_state=42)
+scores = cross_val_score(rfc, data, target, cv = 5)
+print("Cross Validation Scores (Test Size 0.2): ", scores)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+cv = ShuffleSplit(n_splits=3, test_size=0.3, random_state=0)
+scores2 = cross_val_score(rfc, data, target, cv=cv)
+print("Cross Validation Scores (Shuffled, Test Size 0.3)", cross_val_score(rfc, data, target, cv=cv))
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores2.mean(), scores2.std() * 2))
