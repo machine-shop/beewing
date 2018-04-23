14cffb164612c88770613bf7045050180dcbeab2
jiayue-li <jiayue.li@berkeley.edu>
2018-04-23 15:10:19 -0700

    reorganizing classifiers

--- feature_extraction/batch_runner.py
+++ feature_extraction/batch_runner.py
@@ -12,8 +12,14 @@ class BatchRunner:
     def set_path(folder_path):
         self.path = folder_path
 
-    def run_batch(self, f, output_folder=self.output_folder):
-        '''Runs function f on all files in folder_path'''
+    def run_batch(self, f, save=False, output_ext = ".jpg", output_folder="batch_output"):
+        '''
+        Runs function f on all files in folder_path
+        '''
         for root, dirs, files in os.walk("./enhanced_image"):
             for filename in files:
                 out = f(filename)
+                if save:
+                    outfile = open(filename+output_ext, 'w')
+                    outfile.write(filename)
+        
